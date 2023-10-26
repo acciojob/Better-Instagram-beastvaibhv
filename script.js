f0bff38caab4b517c441fbb99fde7e0c8c6d9f34
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-10-26 12:35:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+const image = document.querySelectorAll(".image");
+
+image.addEventListener("dragstart", (event)=>{
+	
+});
+image.addEventListener("dragover", (event)=>{
+	event.preventDefault();
+});
+image.addEventListener("drop", (event)=>{
+	
+});
