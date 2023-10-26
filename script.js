a089a9cea40802303e15c660d9e82be5857f5ee5
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-10-26 13:42:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 //your code here
-const image = document.querySelectorAll(".image");
+// Get all the draggable divs
+const images = document.querySelectorAll('.image');
 
-image.addEventListener("dragstart", (event)=>{
-	
+// Store the currently selected element
+let selectedImage = null;
+
+// Loop through the draggable divs
+images.forEach(image => {
+  image.addEventListener('dragstart', dragStart);
+  image.addEventListener('dragend', dragEnd);
 });
-image.addEventListener("dragover", (event)=>{
-	event.preventDefault();
-});
-image.addEventListener("drop", (event)=>{
-	
-});
+
+// Drag start event
+function dragStart(e) {
+  selectedImage = this;
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+// Drag end event
+function dragEnd() {
+  selectedImage = null;
+}
+
+// Allow dropping on the parent div
+const parent = document.getElementById('parent');
+parent.addEventListener('dragover', allowDrop);
+parent.addEventListener('drop', drop);
+
+// Allow drop event
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+// Drop event
+function drop(e) {
+  e.preventDefault();
+  if (selectedImage) {
+    // Replace the target div's HTML with the selected div's HTML
+    this.innerHTML = e.target.innerHTML;
+    e.target.innerHTML = e.dataTransfer.getData('text/html');
+  }
+}
